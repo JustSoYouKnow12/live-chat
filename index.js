5d30ac5a1ebd1029025777aa7a9085d208d9b912
terry <terry.wang6777@gmail.com>
2026-03-21 16:38:42 +1300

    home page

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+console.log(dirname);
+
+app.use(express.static(path.join(dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/join', (req, res) => {
+    const nickname = req.body.nickname;
+
+    res.send('welcome ' + nickname);
+});
+
+app.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
